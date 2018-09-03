4865443634b8352885fae0cf6e576310db0c3264
Lars <lars.vlyminckx@hotmail.com>
2018-09-03 14:46:44 +0200

    random generated

--- server.js
+++ server.js
@@ -11,11 +11,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+var answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.",
+		"Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy", "try again", "Ask again later.", "Better not tell you now.",
+		"Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no",
+		"Outlook not so good.", "Very doubtful."] ;
+
+
 app.get('/', (req, res) => {
-	res.render('search.ejs')
+	res.render('search.ejs', {result: null});
 })
 
-app.post('/search', (req, res) => {
-//	console.log(req.body.question);
-	res.render('search_result.ejs');
+app.post('/', (req, res) => {
+	
+	question = req.body.question;
+	
+	max = answers.length - 1;
+	min = 0;
+	range = max - min + 1;		
+	rnd = (Math.random() * range) + min;
+	
+	
+	console.log(answers[parseInt(rnd, 10)]);
+	res.render('search.ejs', {result: question});
 })
